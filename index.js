cb639149adb417baf75c002b9c753eb7f51dde84
BuiTranTien <buitrantien1804@gmail.com>
2025-11-26 09:52:10 +0700

    connect to db

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT ||  5000;
+const mongoose = require('mongoose')
+const port = process.env.PORT || 5000;
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+//password vhzUERZmkKa79ttv
+
+
+async function main(params) {
+  await mongoose.connect(process.env.DB_URL)
+  app.use('/', (req, res) => {
+    res.send('Hello World ehllo!')
+  })
+}
+
+main().then(()=> console.log('mongodb connected succuess')).catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
