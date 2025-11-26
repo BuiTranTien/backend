d9510536d6599b128657bcf932b149f59710032e
BuiTranTien <buitrantien1804@gmail.com>
2025-11-26 18:56:30 +0700

    create order api

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const cors = require('cors')
 //middleware
 app.use(express.json());
 app.use(cors({
-  origin:['http://localhost:5173/'],
+  origin:['http://localhost:5173'],
   credentials: true
 }))
 
 //routes
 const bookRoutes = require('./src/books/book.route')
+const orderRoutes = require("./src/orders/order.route")
+
 app.use("/api/books", bookRoutes)
+app.use("/api/orders", orderRoutes)
 
 async function main(params) {
   await mongoose.connect(process.env.DB_URL)
