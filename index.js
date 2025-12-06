3a52bf4588bc409d9b6190773a520cce25961809
BuiTranTien <buitrantien1804@gmail.com>
2025-12-06 21:04:57 +0700

    fix api

--- index.js
+++ index.js
@@ -6,34 +6,39 @@ require('dotenv').config()
 const cors = require('cors')
 
 
-//middleware
-app.use(express.json());
+// CORS middleware
 app.use(cors({
-  origin:['http://localhost:5173'],
-  credentials: true
+  origin: [
+    'http://localhost:5173',
+    'https://readly-frontend-lemon.vercel.app'
+  ],
+  credentials: true,
 }))
 
-//routes
+// ROUTES
 const bookRoutes = require('./src/books/book.route')
 const orderRoutes = require("./src/orders/order.route")
 const userRoutes = require("./src/users/user.route");
 const adminRoutes = require("./src/stats/admin.stats")
-
 
 app.use("/api/books", bookRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
 app.use("/api/admin", adminRoutes)
 
-async function main(params) {
+
+// 👉 ĐÚNG: chỉ match "/", không match các route API
+app.get('/', (req, res) => {
+  res.send('Hello World ehllo!')
+})
+
+async function main() {
   await mongoose.connect(process.env.DB_URL)
-  
-  app.use('/', (req, res) => {
-    res.send('Hello World ehllo!')
-  })
 }
 
-main().then(()=> console.log('mongodb connected succuess')).catch(err => console.log(err));
+main()
+  .then(() => console.log('mongodb connected succuess'))
+  .catch(err => console.log(err))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
